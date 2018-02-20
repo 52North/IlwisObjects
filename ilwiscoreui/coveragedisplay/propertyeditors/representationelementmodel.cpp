a861dfe10cbbcb2e3498cadd1a1962f0e568b74c
mschouwen <m.l.schouwenburg@utwente.nl>
2018-02-20 13:40:36 +0100

    rename from representationelement

---
+++ ilwiscoreui/coveragedisplay/propertyeditors/representationelementmodel.cpp
@@ -0,0 +1,39 @@
+#include "kernel.h"
+#include "ilwisdata.h"
+#include "datadefinition.h"
+#include "columndefinition.h"
+#include "representation.h"
+#include "representationelementmodel.h"
+#include "visualpropertyeditor.h"
+
+using namespace Ilwis;
+using namespace Ui;
+
+RepresentationElementModel::RepresentationElementModel()
+{
+
+}
+
+RepresentationElementModel::RepresentationElementModel(VisualPropertyEditor *p) : QObject(p)
+{
+}
+
+RepresentationElementModel::RepresentationElementModel(const QString &label, VisualPropertyEditor *p) : QObject(p), Identity(label)
+{
+}
+
+RepresentationElementModel::RepresentationElementModel(Raw raw, const QString &label, const QColor &clr, VisualPropertyEditor *parent) : QObject(parent), Identity(label), _color(clr), _raw(raw)
+{
+}
+
+QColor RepresentationElementModel::color() const
+{
+    return _color;
+}
+
+void RepresentationElementModel::color(const QColor &clr)
+{
+    _color = clr;
+    emit ecolorChanged();
+}
+
--- ilwiscoreui/coveragedisplay/propertyeditors/representationelementmodel.h
+++ ilwiscoreui/coveragedisplay/propertyeditors/representationelementmodel.h
@@ -10,14 +10,14 @@ namespace Ilwis {
 namespace Ui{
 class VisualPropertyEditor;
 
-class ILWISCOREUISHARED_EXPORT RepresentationElement : public QObject, public Ilwis::Identity{
+class ILWISCOREUISHARED_EXPORT RepresentationElementModel : public QObject, public Ilwis::Identity{
     Q_OBJECT
 
 public:
-    RepresentationElement();
-    explicit RepresentationElement(VisualPropertyEditor *p);
-    RepresentationElement(const QString& label, VisualPropertyEditor *parent=0);
-    RepresentationElement(Raw raw,const QString& label, const QColor& clr, VisualPropertyEditor *parent=0);
+    RepresentationElementModel();
+    explicit RepresentationElementModel(VisualPropertyEditor *p);
+    RepresentationElementModel(const QString& label, VisualPropertyEditor *parent=0);
+    RepresentationElementModel(Raw raw,const QString& label, const QColor& clr, VisualPropertyEditor *parent=0);
     Q_PROPERTY(QColor ecolor READ color WRITE color NOTIFY ecolorChanged)
     Q_PROPERTY(QString label READ name CONSTANT)
 
