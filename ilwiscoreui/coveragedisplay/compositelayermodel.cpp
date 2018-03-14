fa67d8e13a83b6143ccf7e1cb7159b77c0f20e8b
mschouwen <m.l.schouwenburg@utwente.nl>
2018-03-14 08:18:52 +0100

    rename from intermediate layer

---
+++ ilwiscoreui/coveragedisplay/compositelayermodel.cpp
@@ -0,0 +1,45 @@
+#include "kernel.h"
+#include "ilwisdata.h"
+#include "coverage.h"
+#include "datadefinition.h"
+#include "columndefinition.h"
+#include "table.h"
+#include "layermanager.h"
+#include "representation.h"
+#include "attributemodel.h"
+#include "visualattribute.h"
+#include "compositelayermodel.h"
+
+using namespace Ilwis;
+using namespace Ui;
+
+CompositeLayerModel::CompositeLayerModel() {
+
+}
+CompositeLayerModel::CompositeLayerModel(LayerModel *parent)
+	: LayerModel()
+{
+}
+
+CompositeLayerModel::CompositeLayerModel(LayerManager *manager, QStandardItem *parent, const QString& name, const QString& desc, const IOOptions& opt) : LayerModel(manager, parent, name, desc, opt) {
+	//_layerType = itCOMPOSITELAYER;
+	_isValid = true;
+	_icon = "layers_s_green.png";
+	setEditable(true);
+}
+
+CompositeLayerModel::~CompositeLayerModel()
+{
+}
+
+LayerModel *CompositeLayerModel::create(LayerManager *manager, QStandardItem *parentLayer, const QString &name, const QString &desc, const IOOptions& options)
+{
+	return new CompositeLayerModel(manager, parentLayer, name, desc, options);
+}
+
+void CompositeLayerModel::setActiveAttribute(int idx) {
+}
+
+int CompositeLayerModel::numberOfBuffers(const QString&) const {
+    return 0;
+}
---
+++ ilwiscoreui/coveragedisplay/compositelayermodel.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "coveragedisplay\layermodel.h"
+
+namespace Ilwis {
+
+	namespace Ui {
+		class ILWISCOREUISHARED_EXPORT CompositeLayerModel : public LayerModel
+		{
+			Q_OBJECT
+
+		public:
+			Q_INVOKABLE void setActiveAttribute(int idx);
+
+			CompositeLayerModel();
+			CompositeLayerModel(LayerModel *parent);
+			CompositeLayerModel(LayerManager * manager, QStandardItem * parent, const QString & name, const QString & desc, const IOOptions & opt);
+			~CompositeLayerModel();
+            int numberOfBuffers(const QString&) const;
+
+			static LayerModel *create(LayerManager *manager, QStandardItem *parentLayer, const QString &name, const QString &desc, const IOOptions& options);
+		};
+	}
+}
