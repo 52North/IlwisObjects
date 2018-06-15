8cc6faec30bc8246c459727e0dec09f8378f07f2
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2018-06-15 18:01:42 +0200

    Update plugincode

--- charts/ilwischarts_plugin.cpp
+++ charts/ilwischarts_plugin.cpp
@@ -4,10 +4,24 @@
 
 #include "kernel.h" 
 #include "ilwisdata.h"
+#include "factory.h"
+#include "abstractfactory.h"
+#include "setseriescolor.h"
+#include "chartoperationfactory.h"
+
+using namespace Ilwis;
+using namespace Ui;
 
 void ChartsPlugin::registerTypes(const char *uri)
 {
-    Ilwis::kernel()->issues()->log("Loaded module Ilwis Charts",Ilwis::IssueObject::itMessage);
+/*    ChartOperationFactory *factory = new ChartOperationFactory();
+    factory->registerChartOperation("setseriescolor", SetSeriesColor::create);
+//    factory->registerTableOperation("convertcolumndomain", ConvertColumnDomain::create);
+//    factory->registerTableOperation("columnvisibility", ColumnVisibility::create);
+//    factory->registerTableOperation("linktable", LinkTable::create);
+    Ilwis::kernel()->addFactory(factory);
+*/
+    Ilwis::kernel()->issues()->log("Loaded Ilwis Charts module",Ilwis::IssueObject::itMessage);
 }
 
 
