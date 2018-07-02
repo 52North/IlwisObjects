af1b4ba54bcb6d66698cbba3ea62a12d354bd0fc
mschouwen <m.l.schouwenburg@utwente.nl>
2018-07-02 13:39:53 +0200

    added methods for determining if a histogram is calculated and translate a histogram to a table. The table will not contain the undefs. For this you have to used the bins from the statistics().histogram function

--- core/ilwisobjects/coverage/rastercoverage.h
+++ core/ilwisobjects/coverage/rastercoverage.h
@@ -209,6 +209,7 @@ public:
     void setPseudoUndef(double v);
 
     bool canUse(const IlwisObject *obj, bool strict=false) const ;
+    bool histogramCalculated() const;
     ITable histogramAsTable() ;
 
 protected:
