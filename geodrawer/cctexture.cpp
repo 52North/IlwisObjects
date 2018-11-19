f84f8e6934f75e156024bb020bd072ceb55ee65b
schouwenburg <schouwenburg@UTWKS61938>
2018-11-19 14:45:03 +0100

    ADAPTED FOR COLORCOMPISTE

--- geodrawer/cctexture.cpp
+++ geodrawer/cctexture.cpp
@@ -9,8 +9,9 @@
 using namespace Ilwis;
 using namespace Ui;
 
-CCTexture::CCTexture(RasterLayerModel * rasterLayerModel, const IRasterCoverage & raster, const long offsetX, const long offsetY, const unsigned long sizeX, const unsigned long sizeY, unsigned int zoomFactor)
-: Texture(rasterLayerModel, raster, offsetX, offsetY, sizeX, sizeY, zoomFactor, 0)
+CCTexture::CCTexture(RasterLayerModel * rasterLayerModel, const std::vector<IRasterCoverage> ccBands, const long offsetX, const long offsetY, const unsigned long sizeX, const unsigned long sizeY, unsigned int zoomFactor)
+: Texture(rasterLayerModel, ccBands[0], offsetX, offsetY, sizeX, sizeY, zoomFactor, 0)
+, _ccBands(ccBands)
 {
 }
 
@@ -30,8 +31,8 @@ void CCTexture::ReCreateTexture(bool fInThread, volatile bool * fDrawStop)
 
 bool CCTexture::DrawTexture(long offsetX, long offsetY, long texSizeX, long texSizeY, unsigned int zoomFactor, QVector<int> & texture_data, volatile bool* fDrawStop)
 {
-    long imageWidth = _raster->size().xsize();
-    long imageHeight = _raster->size().ysize();
+    long imageWidth = _ccBands[0]->size().xsize();
+    long imageHeight = _ccBands[0]->size().ysize();
     long sizeX = texSizeX; // the size of the input (pixeliterator)
     long sizeY = texSizeY;
     if (offsetX + sizeX > imageWidth)
@@ -47,10 +48,9 @@ bool CCTexture::DrawTexture(long offsetX, long offsetY, long texSizeX, long texS
     if (*fDrawStop)
         return false;
 
-    std::vector<qint32> & bandNrs = _rasterLayerModel->ccBands();
-    BoundingBox bbR(Pixel(offsetX, offsetY, bandNrs[0]), Pixel(offsetX + sizeX - 1, offsetY + sizeY - 1, bandNrs[0]));
-    BoundingBox bbG(Pixel(offsetX, offsetY, bandNrs[1]), Pixel(offsetX + sizeX - 1, offsetY + sizeY - 1, bandNrs[1]));
-    BoundingBox bbB(Pixel(offsetX, offsetY, bandNrs[2]), Pixel(offsetX + sizeX - 1, offsetY + sizeY - 1, bandNrs[2]));
+    BoundingBox bbR(Pixel(offsetX, offsetY, 0), Pixel(offsetX + sizeX - 1, offsetY + sizeY - 1, 0));
+    BoundingBox bbG(Pixel(offsetX, offsetY, 0), Pixel(offsetX + sizeX - 1, offsetY + sizeY - 1, 0));
+    BoundingBox bbB(Pixel(offsetX, offsetY, 0), Pixel(offsetX + sizeX - 1, offsetY + sizeY - 1, 0));
     quint32 size = texSizeX * texSizeY * 4; // r,g,b,a
     if (texture_data.size() == 0)
         texture_data.resize(size); // allowed the first time only; after this the vector will always be in-use by a webGL object
@@ -58,55 +58,65 @@ bool CCTexture::DrawTexture(long offsetX, long offsetY, long texSizeX, long texS
     if (*fDrawStop)
         return false;
 
-    VisualAttribute * attr = _rasterLayerModel->activeAttribute();
-    if (attr != 0) {
-        NumericRange & actualRange = attr->actualRange();
-        NumericRange & stretchRange = attr->stretchRange();
-        quint32 position = 0; // red
-        PixelIterator pixIterR(_raster, bbR); // This iterator runs through bbR. The corners of bbR are "inclusive".
-        auto endR = pixIterR.end();
-        while (pixIterR != endR && position < size) {
-            double valueR = *pixIterR;
-            int colorR = isNumericalUndef2(valueR, _raster) ? 0 : 255.0 * getStretchedValue(valueR, actualRange, stretchRange);
-            texture_data[position] = colorR;
-            position += 3;
-            texture_data[position++] = 255; // alpha
-            pixIterR += zoomFactor;
-            if (pixIterR.ychanged()) {
-                if (*fDrawStop)
-                    return false;
+    VisualAttribute * attr = _rasterLayerModel->layer(0)->activeAttribute();
+	if (attr != 0) {
+		NumericRange & actualRange = attr->actualRange();
+		NumericRange & stretchRange = attr->stretchRange();
+		quint32 position = 0; // red
+		PixelIterator pixIterR(_ccBands[0], bbR); // This iterator runs through bbR. The corners of bbR are "inclusive".
+		auto endR = pixIterR.end();
+		while (pixIterR != endR && position < size) {
+			double valueR = *pixIterR;
+			int colorR = isNumericalUndef2(valueR, _ccBands[0]) ? 0 : 255.0 * getStretchedValue(valueR, actualRange, stretchRange);
+			texture_data[position] = colorR;
+			position += 3;
+			texture_data[position++] = 255; // alpha
+			pixIterR += zoomFactor;
+			if (pixIterR.ychanged()) {
+				if (*fDrawStop)
+					return false;
 
-                position += 4 * (texSizeX - xSizeOut);
-                if (zoomFactor > 1) {
-                    pixIterR += sizeX * (zoomFactor - 1) - ((zoomFactor - (sizeX % zoomFactor)) % zoomFactor);
-                }
-            }
-        }
-        position = 1; // green
-        PixelIterator pixIterG(_raster, bbG); // This iterator runs through bbG. The corners of bbG are "inclusive".
-        auto endG = pixIterG.end();
-        while (pixIterG != endG && position < size) {
-            double valueG = *pixIterG;
-            int colorG = isNumericalUndef2(valueG, _raster) ? 0 : 255.0 * getStretchedValue(valueG, actualRange, stretchRange);
-            texture_data[position] = colorG;
-            position += 4;
-            pixIterG += zoomFactor;
-            if (pixIterG.ychanged()) {
-                if (*fDrawStop)
-                    return false;
+				position += 4 * (texSizeX - xSizeOut);
+				if (zoomFactor > 1) {
+					pixIterR += sizeX * (zoomFactor - 1) - ((zoomFactor - (sizeX % zoomFactor)) % zoomFactor);
+				}
+			}
+		}
+	}
+	attr = _rasterLayerModel->layer(1)->activeAttribute();
+	if (attr != 0) {
+		NumericRange & actualRange = attr->actualRange();
+		NumericRange & stretchRange = attr->stretchRange();
+		quint32 position = 1; // green
+		PixelIterator pixIterG(_ccBands[1], bbG); // This iterator runs through bbG. The corners of bbG are "inclusive".
+		auto endG = pixIterG.end();
+		while (pixIterG != endG && position < size) {
+			double valueG = *pixIterG;
+			int colorG = isNumericalUndef2(valueG, _ccBands[1]) ? 0 : 255.0 * getStretchedValue(valueG, actualRange, stretchRange);
+			texture_data[position] = colorG;
+			position += 4;
+			pixIterG += zoomFactor;
+			if (pixIterG.ychanged()) {
+				if (*fDrawStop)
+					return false;
 
-                position += 4 * (texSizeX - xSizeOut);
-                if (zoomFactor > 1) {
-                    pixIterG += sizeX * (zoomFactor - 1) - ((zoomFactor - (sizeX % zoomFactor)) % zoomFactor);
-                }
-            }
-        }
-        position = 2; // blue
-        PixelIterator pixIterB(_raster, bbB); // This iterator runs through bbB. The corners of bbB are "inclusive".
+				position += 4 * (texSizeX - xSizeOut);
+				if (zoomFactor > 1) {
+					pixIterG += sizeX * (zoomFactor - 1) - ((zoomFactor - (sizeX % zoomFactor)) % zoomFactor);
+				}
+			}
+		}
+	}
+	attr = _rasterLayerModel->layer(2)->activeAttribute();
+	if (attr != 0) {
+		NumericRange & actualRange = attr->actualRange();
+		NumericRange & stretchRange = attr->stretchRange();
+		quint32 position = 2; // blue
+        PixelIterator pixIterB(_ccBands[2], bbB); // This iterator runs through bbB. The corners of bbB are "inclusive".
         auto endB = pixIterB.end();
         while (pixIterB != endB && position < size) {
             double valueB = *pixIterB;
-            int colorB = isNumericalUndef2(valueB, _raster) ? 0 : 255.0 * getStretchedValue(valueB, actualRange, stretchRange);
+            int colorB = isNumericalUndef2(valueB, _ccBands[2]) ? 0 : 255.0 * getStretchedValue(valueB, actualRange, stretchRange);
             texture_data[position] = colorB;
             position += 4;
             pixIterB += zoomFactor;
--- geodrawer/cctexture.h
+++ geodrawer/cctexture.h
@@ -8,12 +8,13 @@ namespace Ilwis {
 	    class CCTexture : public Texture
 	    {
 	    public:
-            CCTexture(RasterLayerModel * rasterLayerModel, const IRasterCoverage & raster, const long offsetX, const long offsetY, const unsigned long sizeX, const unsigned long sizeY, unsigned int zoomFactor);
+            CCTexture(RasterLayerModel * rasterLayerModel, const std::vector<IRasterCoverage> ccBands, const long offsetX, const long offsetY, const unsigned long sizeX, const unsigned long sizeY, unsigned int zoomFactor);
             virtual ~CCTexture();
             virtual void CreateTexture(bool fInThread, volatile bool * fDrawStop);
             virtual void ReCreateTexture(bool fInThread, volatile bool * fDrawStop);
         protected:
             bool DrawTexture(long offsetX, long offsetY, long texSizeX, long texSizeY, unsigned int zoomFactor, QVector<int> & texture_data, volatile bool* fDrawStop);
+			std::vector<IRasterCoverage> _ccBands;
 	    };
 	}
 }
