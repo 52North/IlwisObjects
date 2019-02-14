aa7d436c5791a7b3ef7ca6def101827bfd5f357f
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2019-02-14 14:15:50 +0100

    Workflow "save as" now uses new filename as display name

--- streamconnector/versionedserializer.cpp
+++ streamconnector/versionedserializer.cpp
@@ -112,7 +112,7 @@ bool VersionedSerializer::store(IlwisObject *obj, const IOOptions &options)
 {
     double mtime = (double)obj->modifiedTime();
     double ctime = (double)obj->createTime();
-    Resource res = obj->resource();
+    Resource res = obj->resource(IlwisObject::cmOUTPUT);
     QString nm =  res.hasProperty("longname") ? obj->name() + "|" + res["longname"].toString() : obj->name();
     _stream <<  obj->ilwisType() << Version::interfaceVersion << obj->extendedType() << nm << obj->code() << obj->description() << obj->isReadOnly()  << mtime << ctime;
 
