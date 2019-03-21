ddeeef85fb49901b44d9f442f9e9227c3e574b7d
retsios <v.retsios@utwente.nl>
2019-03-21 19:23:08 +0100

    Correction in addBand() against accidental double addition of new band index; function RasterCoverage::band() already registers the new band data, if the index is rUNDEF or sUNDEF.

--- pythonapi/pythonapi_rastercoverage.cpp
+++ pythonapi/pythonapi_rastercoverage.cpp
@@ -552,14 +552,13 @@ PixelIterator RasterCoverage::band(PyObject *pyTrackIndex){
 
 void RasterCoverage::addBand(PyObject* pyTrackIndex, PixelIterator* pyIter){
     QVariant qIndex = resolveIndex(pyTrackIndex);
-    Ilwis::DataDefinition& ilwdef = this->ptr()->as<Ilwis::RasterCoverage>()->datadefRef();
     if((QMetaType::Type)qIndex.type() == QMetaType::Double){
-        this->_ilwisObject->as<Ilwis::RasterCoverage>()->stackDefinitionRef().insert(qIndex.toDouble());
-        this->_ilwisObject->as<Ilwis::RasterCoverage>()->setBandDefinition(qIndex.toDouble(), ilwdef);
+        if (qIndex.toDouble() != rUNDEF)
+            this->_ilwisObject->as<Ilwis::RasterCoverage>()->stackDefinitionRef().insert(qIndex.toDouble());
         this->_ilwisObject->as<Ilwis::RasterCoverage>()->band(qIndex.toDouble(), pyIter->ptr());
     } else if((QMetaType::Type)qIndex.type() == QMetaType::QString){
-        this->_ilwisObject->as<Ilwis::RasterCoverage>()->stackDefinitionRef().insert(qIndex.toString());
-        this->_ilwisObject->as<Ilwis::RasterCoverage>()->setBandDefinition(qIndex.toString(), ilwdef);
+        if (qIndex.toString() != sUNDEF)
+            this->_ilwisObject->as<Ilwis::RasterCoverage>()->stackDefinitionRef().insert(qIndex.toString());
         this->_ilwisObject->as<Ilwis::RasterCoverage>()->band(qIndex.toString(), pyIter->ptr());
     } else {
         throw InvalidObject("The index is not included in the definition.");
