9829b34b42c83215b20b31939527c2a5b19484f6
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2019-08-22 15:32:23 +0200

    works

--- ilwisscript/parserlexer/renfiles.bat
+++ ilwisscript/parserlexer/renfiles.bat
@@ -1,4 +1,4 @@
 del IlwisScriptParser.cpp
 del IlwisScriptLexer.cpp
-ren IlwisScriptParser.c IlwisScriptParser.cpp
-ren IlwisScriptLexer.c IlwisScriptLexer.cpp
+ren ./output/IlwisScriptParser.c ./IlwisScriptParser.cpp
+ren ./output/IlwisScriptLexer.c ./IlwisScriptLexer.cpp
