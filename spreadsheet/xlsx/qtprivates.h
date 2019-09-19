73db25b0ae012e2a32ecb397ba92175d631c0677
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2019-09-19 08:38:31 +0200

    new spreadsheet connector; there are some private links to qt internals in the original code . Not a good solution but this is how the original programmer did it

---
+++ spreadsheet/xlsx/qtprivates.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <5.12.2\QtGui\private\qtguiglobal_p.h>
+#include <5.12.2\QtGui\private\qtgui-config_p.h>
+#include <5.12.2\QtGui\private\qzipwriter_p.h>
+#include <5.12.2\QtGui\private\qzipreader_p.h>
