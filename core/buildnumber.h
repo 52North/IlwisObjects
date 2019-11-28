4d14ed9d5a10f71afa2b40f73833874eac60b234
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2019-11-28 12:00:16 +0100

    upped

--- core/buildnumber.h
+++ core/buildnumber.h
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 #ifndef BUILDNR_H
 #define BUILDNR_H
-#define ILWIS_BUILDNR 201911141200
+#define ILWIS_BUILDNR 201911271400
 #define ILWIS_VERSION_TYPE "Alpha"
 #define ILWIS_VERSION_NUMBER 28
 #endif
