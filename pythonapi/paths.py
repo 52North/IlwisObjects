5577ec2b271faf097260e5ac6c881dabb17433e5
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2022-02-07 11:38:42 +0100

    different location for the extension, always under python . Maybe made more flexible later.

--- pythonapi/paths.py
+++ pythonapi/paths.py
@@ -1,11 +1,4 @@
 import os
-path = os.path.dirname(__file__)
-if (len(path) > 0):
-    path += os.sep
-f = open(path + 'ilwislocation.config', 'r')
-f.readline()
-path = f.readline().strip()
-path = path.split('=')[1]
-f.close()
+path = os.path.dirname(__file__) + '\\ilwisobjects'
 os.environ["PATH"] = path + os.pathsep + os.environ["PATH"]
 
