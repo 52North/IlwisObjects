36f747355df6c2c446cc1632fe732803553de39c
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2022-08-24 11:24:41 +0200

    Implemented "casts" for ranges

--- pythonapi/pythonapi_range.h
+++ pythonapi/pythonapi_range.h
@@ -8,16 +8,28 @@
 #include <QSharedPointer>
 #include "pythonapi_util.h"
 
+typedef std::shared_ptr<Ilwis::Range> SPPRange;
+
 namespace Ilwis {
 class Range;
 class NumericRange;
 class DomainItem;
 typedef QSharedPointer<DomainItem> SPDomainItem;
+ class IntervalRange;
+ class ColorPalette;
 }
 
 typedef struct _object PyObject;
 
 namespace pythonapi {
+    class NumericRange;
+    class TimeInterval;
+    class ColorPalette;
+    class ContinuousColorRange;
+    class NamedItemRange;
+    class ThematicRange;
+    class IndexedItemRange;
+    class NumericItemRange;
 
 class Range: public Object{
     friend class Domain;
@@ -26,6 +38,8 @@ class Range: public Object{
     template<typename OutputType, typename RangeType, typename IlwOutput,  typename IlwRange>
     friend class RangeIterator;
     friend class Interval;
+
+
 
 public:
     bool __bool__() const;
@@ -38,9 +52,17 @@ public:
 
     bool isContinuous() const;
     PyObject* impliedValue(const PyObject *value) const;
+    NumericRange *toNumericRange();
+    pythonapi::NumericItemRange *toNumericItemRange();
+    TimeInterval *toTimeInterval();
+    ColorPalette *toColorPalette();
+    ContinuousColorRange *toContinuousColorRange();
+    ThematicRange *toThematicRangee();
+    NamedItemRange *toNamedItemRange();
+    IndexedItemRange *toIndexedItemRange();
 
 protected:
-    std::shared_ptr<Ilwis::Range> _range;
+    SPPRange _range;
     Range(Ilwis::Range *rng);
 
     Range();
@@ -76,16 +98,20 @@ public:
     void clear();
 };
 
-class ItemRange : public virtual Range {
+class ItemRange : public Range {
 public:
     virtual void add(PyObject *dItem) = 0;
     quint32 count();
     void remove(const std::string& name);
     void clear();
+protected:
+    ItemRange(Ilwis::ItemRange* rng)    ;
+    ItemRange();
 };
 
 class NumericItemRange : public ItemRange{
 public:
+    NumericItemRange(Ilwis::IntervalRange* rng);
     NumericItemRange();
     void add(std::string name, double min, double max, double resolution=0);
     void add(PyObject *item);
@@ -106,6 +132,7 @@ public:
 class NamedItemRange : public ItemRange {
 public:
     NamedItemRange();
+    NamedItemRange(Ilwis::ItemRange *rng);
     void add(PyObject *item);
     PyObject* listAll();
     qint32 gotoIndex(qint32 index, qint32 step) const;
@@ -115,6 +142,7 @@ public:
 class ThematicRange : public ItemRange {
 public:
     ThematicRange();
+    ThematicRange(Ilwis::ItemRange *rng);
     void add(std::string name, std::string id="", std::string descr="");
     void add(PyObject *item);
     PyObject* listAll();
@@ -122,13 +150,13 @@ public:
 };
 
 
-class ColorRange : public virtual Range{
+class ColorRangeBase{
     friend class ColorDomain;
     friend class ItemDomain;
 
 public:
-    ColorRange();
-    ColorRange(IlwisTypes tp, ColorModel clrmodel);
+    ColorRangeBase();
+    ColorRangeBase(IlwisTypes tp, ColorModel clrmodel);
     ColorModel defaultColorModel() const;
     void defaultColorModel(ColorModel m);
 
@@ -136,8 +164,9 @@ public:
     static Color toColor(PyObject* v, ColorModel colortype, const std::string& name = "");
 
 protected:
-    Color qColorToColor(QColor qCol, const std::string& name = "") const;
+    Color qColorToColor( QColor qCol, const std::string& name = "") const;
     QColor colorToQColor(const Color& pyCol) const;
+    SPPRange _colorRange;
 
 private:
     std::string toString(const Color &clr, ColorModel clrType);
@@ -146,21 +175,23 @@ private:
     ColorModel _defaultModel = ColorModel::cmRGBA;
 };
 
-class ContinuousColorRange : public ColorRange{
+class ContinuousColorRange : public ColorRangeBase, Range{
 public:
     ContinuousColorRange();
+    ContinuousColorRange(Ilwis::Range *rng);
     ContinuousColorRange(const Color& clr1, const Color& clr2);
     ContinuousColorRange *clone() const;
     PyObject* ensure(const PyObject *v, bool inclusive = true) const;
     bool containsVar(const PyObject *v, bool inclusive = true) const;
     bool containsColor(const Color &clr, bool inclusive = true) const;
-    bool containsRange(ColorRange *v, bool inclusive = true) const;
+    bool containsRange(ColorRangeBase *v, bool inclusive = true) const;
     Color impliedValue(const PyObject* v) const;
 };
 
-class ColorPalette : public ItemRange, public ColorRange{
+class ColorPalette : public ItemRange, public ColorRangeBase{
 public:
     ColorPalette();
+    ColorPalette(Ilwis::ColorPalette *rng);
 
     Color item(quint32 raw) const;
     Color item(const std::string& name) const;
@@ -173,7 +204,7 @@ public:
     void clear();
 
     bool containsColor(const Color &clr, bool inclusive = true) const;
-    bool containsRange(ColorRange *v, bool inclusive = true) const;
+    bool containsRange(ColorRangeBase *v, bool inclusive = true) const;
 
     quint32 count();
 
@@ -183,8 +214,8 @@ public:
 private:
     void add(PyObject* item);
     Color itemToColor(Ilwis::SPDomainItem item) const;
-    std::shared_ptr<Ilwis::Range> _rangeColor = pythonapi::ColorRange::_range;
-    std::shared_ptr<Ilwis::Range> _rangeItem = pythonapi::ItemRange::_range;
+   // std::shared_ptr<Ilwis::Range> _rangeColor = pythonapi::ColorRange::_range;
+   // std::shared_ptr<Ilwis::Range> _rangeItem = pythonapi::ItemRange::_range;
 };
 
 class TimeInterval : public NumericRange{
