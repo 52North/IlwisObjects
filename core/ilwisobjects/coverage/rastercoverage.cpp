ccd0ddfbce517df135ba147c8a3347f363c54ddf
retsios <v.retsios@utwente.nl>
2023-07-14 14:55:17 +0200

    Added copying the _recordLookup member in the copyTo() operation. Otherwise if the rastercoverage has an attribute table, the function raw2record() will not work after copying.

--- core/ilwisobjects/coverage/rastercoverage.cpp
+++ core/ilwisobjects/coverage/rastercoverage.cpp
@@ -570,6 +570,7 @@ void RasterCoverage::copyTo(IlwisObject *obj)
     raster->_attributeTable = _attributeTable;
     raster->_size = _size;
     raster->_primaryKey = _primaryKey;
+    raster->_recordLookup = _recordLookup;
 
 }
 
