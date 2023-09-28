b87d602103f6c527a0dc3479bd46769d6b590bc4
retsios <v.retsios@utwente.nl>
2023-09-28 17:11:35 +0200

    Added missing extension "dom" when storing a class domain name in the odf of a rastercoverage.

--- ilwis3connector/coverageconnector.cpp
+++ ilwis3connector/coverageconnector.cpp
@@ -347,7 +347,7 @@ bool CoverageConnector::storeMetaData(IlwisObject *obj, IlwisTypes type, const I
     }
     if ( dom->ilwisType() == itITEMDOMAIN) {
          if ( hasType(dom->valueType(),itTHEMATICITEM | itNUMERICITEM) && coverage->ilwisType() == itRASTER) {
-            _domainName =  Resource::toLocalFile(dom->resource().url(true), true);
+            _domainName =  Resource::toLocalFile(dom->resource().url(true), true, "dom");
             if ( _domainName == sUNDEF){
                 if ( baseName != sUNDEF)
                     _domainName = QFileInfo(baseName).baseName() + ".dom";
