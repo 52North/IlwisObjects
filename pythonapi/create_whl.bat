97323cfcc6c5a98c54dd01f2a5dd3150d2a60147
retsios <v.retsios@utwente.nl>
2025-02-10 18:42:41 +0100

    Removing backslash solves incompatibility of xcopy on github actions

--- pythonapi/create_whl.bat
+++ pythonapi/create_whl.bat
@@ -7,7 +7,7 @@ mkdir %WHEELFOLDER%
 copy setup.py %WHEELFOLDER%
 copy ..\core\version.cpp %WHEELFOLDER%
 cd %WHEELFOLDER%
-xcopy /s ..\%EXTENSIONFOLDER%\ .
+xcopy /s ..\%EXTENSIONFOLDER% .
 rem for now remove opencv; needs more testing
 rd /q/s ilwis\ilwisobjects\extensions\opencv
 %PYTHONROOT%\python.exe ./setup.py bdist_wheel
