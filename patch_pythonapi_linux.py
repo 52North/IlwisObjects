76e4753c02b4139894c32cb5f282fdd87c20636e
retsios <v.retsios@utwente.nl>
2025-02-12 16:58:30 +0100

    github actions: linux: add detection of python .so file and folder.

--- patch_pythonapi_linux.py
+++ patch_pythonapi_linux.py
@@ -1,29 +1,54 @@
-from sysconfig import get_paths
-from platform import python_version
+import os
+from sysconfig import get_paths, get_config_var
+import ctypes
+from ctypes.util import find_library
+
+class Dl_info(ctypes.Structure):
+    _fields_ = [
+        ("dli_fname", ctypes.c_char_p),
+        ("dli_fbase", ctypes.c_void_p),
+        ("dli_sname", ctypes.c_char_p),
+        ("dli_saddr", ctypes.c_void_p),
+    ]
 
 info = get_paths()
 include_path = info['include']
-version = python_version()
-version = '.'.join(version.split('.')[0:2])
-f = open('pythonapi.pro')
-lines = f.readlines()
-f.close()
-f = None
-lines = [line.rstrip() for line in lines]
-i = 0
-f = open('pythonapi.pro', 'w')
-while i < len(lines):
-    line = lines[i]
-    if line.strip() == 'PYMINORVERSION=8':
-        f.write(line + '\n')
-        i = i + 1
+
+LDLIBRARY = get_config_var("LDLIBRARY")
+if LDLIBRARY.endswith('.a'):
+    LDLIBRARY = LDLIBRARY.rstrip('.a') + '.so'
+libpython = ctypes.CDLL(LDLIBRARY)
+libdl = ctypes.CDLL(find_library("dl"))
+dlinfo = Dl_info()
+retcode = libdl.dladdr(
+    ctypes.cast(libpython.Py_GetVersion, ctypes.c_void_p),
+    ctypes.pointer(dlinfo),
+)
+if retcode != 0:
+    fullpath = os.path.realpath(dlinfo.dli_fname.decode())
+    fullpath = fullpath.split('.so')[0]
+    fullpath = fullpath.split('/')
+    lib = '/'.join(fullpath[0:-1])
+    name = fullpath[-1].lstrip('lib')
+    f = open('pythonapi.pro')
+    lines = f.readlines()
+    f.close()
+    f = None
+    lines = [line.rstrip() for line in lines]
+    i = 0
+    f = open('pythonapi.pro', 'w')
+    while i < len(lines):
         line = lines[i]
-        while line.strip() != 'OUTPUTPATHPYTHON=$$clean_path($$PWD/../output/$$PREFIX_COMPILER/$$CONF/ilwispy/ilwis)':
+        if line.strip() == 'PYMINORVERSION=8':
+            f.write(line + '\n')
             i = i + 1
             line = lines[i]
-        f.write('    PYTHON_INCLUDE_FILES=' + include_path + '\n')
-        f.write('    LIBS += -L$$/usr/libs -lpython' + version + '\n')
-    f.write(line + '\n')
-    i = i + 1
-f.flush()
-f.close()
+            while line.strip() != 'OUTPUTPATHPYTHON=$$clean_path($$PWD/../output/$$PREFIX_COMPILER/$$CONF/ilwispy/ilwis)':
+                i = i + 1
+                line = lines[i]
+            f.write('    PYTHON_INCLUDE_FILES=' + include_path + '\n')
+            f.write('    LIBS += -L' + lib + ' -l' + name + '\n')
+        f.write(line + '\n')
+        i = i + 1
+    f.flush()
+    f.close()
